fc228918c92648c12a7c3007be4a7d94a4540885
syedhassanhussain <syedhassanhussain001@gmail.com>
2023-06-13 20:11:03 +0500

    summary done

--- src/components/IndexGraph.js
+++ src/components/IndexGraph.js
@@ -11,49 +11,37 @@ const IndexGraph = () => {
       <Plot
         style={{ Width: "100%" }}
         data={[
-          {
-            x: [0, 1, 2, 3, 4, 5, 6, 7, 8],
-            y: [1, 5, 6, 54, 12, 14, 45, 90],
-            marker: { color: "red" },
-            name: "Bonus Regular",
-            type: "scatter",
-          },
-          {
-            x: [0, 1, 2, 3, 4, 5, 6, 7, 8],
-            y: [14, 22, 35, 47, 53, 61, 72, 85],
-            marker: { color: "orange" },
-            name: "Bonus Tristar",
-            type: "scatter",
-          },
-          {
-            x: [0, 1, 2, 3, 4, 5, 6, 7, 8],
-            y: [1, 2, 36, 45, 5, 64, 7, 45],
-            marker: { color: "blue" },
-            name: "Brite Maximum power",
-            type: "scatter",
-          },
-          {
-            x: [0, 1, 2, 3, 4, 5, 6, 7, 8],
-            y: [1, 29, 36, 43, 5, 46, 67, 18],
-            marker: { color: "Purple" },
-            name: "Great Regular Flovour",
-            type: "scatter",
-          },
-          {
-            x: [0, 1, 2, 3, 4, 5, 6, 7, 8],
-            y: [1, 25, 37, 44, 65, 46, 37, 81],
-            marker: { color: "green" },
-            name: "Max Bar Regular",
-            type: "scatter",
-          },
+            {
+                x: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+                y: [14, 22, 35, 47, 53, 61, 72, 85],
+                marker: { color: "blue" },
+                name: "Line of Equality",
+                type: "scatter",
+                // title: 'hello'
+            },
+            {
+              x: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+              y: [1, 5, 6, 54, 12, 14, 45, 90],
+              marker: { color: "red" },
+              name: "Running sum of % Demand",
+              type: "scatter",
+              // title: 'hello'
+            },
         ]}
         layout={
           {
+            yaxis: {
+                title: 'Percent of Total',
+            },
+            xaxis: {
+                title: 'Time Step'
+            }
             // width: '100%',
             // height: 600,
             // title: "hello",
           }
         }
+        config={{responsive: true}}
       />
     </Card>
   );
